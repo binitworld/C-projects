c8db9e87e53de68bebec4f3a5c47d6d01e488030
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-15 02:28:18 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+def add(a, b):
+    return a + b
+
+result = add(a, b)
+print("The result of adding", a, "and", b, "is:", result)
